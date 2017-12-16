0476092ba5b0d3390e95c9a996ef8143bc9c6529
pavanisura <pavani.sura267@gmail.com>
2017-12-16 13:47:45 +0530

    commiting maven project

---
+++ src/main/java/GoJenkins/SampleProject/App.java
@@ -0,0 +1,13 @@
+package GoJenkins.SampleProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
